48f64d34d7c9955e6fa743562491ed2f8f42acea
Alberto Harres <alberto.harres@gmail.com>
2022-08-02 19:30:29 +0400

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Edit Wars - Installation Simulation
+
---
+++ js/script.js
@@ -0,0 +1,19 @@
+
+$(document).ready(function () {
+
+  const assets = document.querySelector('a-assets'); // jquery elements
+  console.log("assets", assets)
+  assets.addEventListener('loaded', evt => {
+    console.log('loaded', evt);
+    loaded = true;
+  })
+
+  var started = false;
+  var video = document.querySelector("video")
+  
+  window.addEventListener("mousedown", function () {
+    if (started) return;
+    started = true;
+    video.play()
+  })
+}) 
